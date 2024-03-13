e45246cf856d24f279debc4da2568219e23acc6f
Nambi Sankaran <nambisankaran@lucidmotors.com>
2024-03-12 23:29:40 -0700

    excluded metrics from the authenticated pages

--- src/main/java/org/github/snambi/restauth/SecurityConfig.java
+++ src/main/java/org/github/snambi/restauth/SecurityConfig.java
@@ -3,7 +3,9 @@ package org.github.snambi.restauth;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -13,30 +15,36 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.List;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//
-//        // .csrf().httpBasic are needed for POST requests to work
-//        //
-//        //http.csrf(AbstractHttpConfigurer::disable);
-////        http.authorizeHttpRequests(
-////                (authzRequests) ->
-////                        authzRequests
-////                                .requestMatchers("/search/**")
-////                                .permitAll()
-////                                .requestMatchers(HttpMethod.POST, "/create/**")
-////                                .permitAll()
-////                                .requestMatchers("/error/**")
-////                                .permitAll()
-////        );
-//
-//
-//        http.cors(x -> x.configurationSource(corsConfigurationSource()));
-//
-//        return http.build();
-//    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // .csrf().httpBasic are needed for POST requests to work
+        //
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(
+                (authzRequests) ->
+                        authzRequests
+                                .requestMatchers("/search/**")
+                                .permitAll()
+                                .requestMatchers(HttpMethod.POST, "/create/**")
+                                .permitAll()
+                                .requestMatchers("/error/**")
+                                .permitAll()
+                                .requestMatchers(HttpMethod.GET, "/actuator/**")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+        );
+
+        http.oauth2ResourceServer( (oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+
+        http.cors(x -> x.configurationSource(corsConfigurationSource()));
+
+        return http.build();
+    }
 
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
